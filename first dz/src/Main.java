f05c12071df51f6c53cd72d5faa2fc8b5b878880
nk <nkonop@softserveinc.com>
2024-03-20 13:12:05 +0200

    dz-5 with the StarUpdate

--- first dz/src/Main.java
+++ first dz/src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         //System.out.println("This is my 1st main class");}
-
+        //System.out.println("This is my 2st main class");}
 
         PrintClass myPrint = new PrintClass();
         myPrint.print();
